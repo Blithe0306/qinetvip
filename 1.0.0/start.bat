e9dc6f2bda8b9f57e9bd4c3a27f8e6268147a1c6
Blithe0306 <lizhanwulele@126.com>
2021-06-17 20:02:48 +0800

    修改bug

---
+++ 1.0.0/start.bat
@@ -0,0 +1,22 @@
+start bee.exe start --config bee1.yaml
+
+start bee.exe start --config bee2.yaml
+
+start bee.exe start --config bee3.yaml
+
+start bee.exe start --config bee4.yaml
+
+start bee.exe start --config bee5.yaml
+
+start bee.exe start --config bee6.yaml
+
+start bee.exe start --config bee7.yaml
+
+start bee.exe start --config bee8.yaml
+
+start bee.exe start --config bee9.yaml
+
+start bee.exe start --config bee10.yaml
+
+
+pause
